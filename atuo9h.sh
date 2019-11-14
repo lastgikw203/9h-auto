a4b2d839f4b80e77dcf3be71fab6fb15a3653264
lastgikw203 <55953948+lastgikw203@users.noreply.github.com>
2019-11-14 10:36:52 +0800

    Create atuo9h.sh

---
+++ atuo9h.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/MauroS5/9Hits-AutoInstall.git &&
+chmod -R 777 9Hits-AutoInstall &&
+9Hits-AutoInstall/install.sh "2" "64730c0a01d3e9cec964b0b03b5c142a" "5" "15" &&
+rm install.sh
